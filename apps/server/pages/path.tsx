498a353f964f63adda01c9715b6fef8fb29154ec
DJ Johnson <djohnson@squareup.com>
2022-09-05 12:32:46 -0700

    chore Server: add path route to help verify what url is being used on the server
    add url examples for example files

---
+++ apps/server/pages/path.tsx
@@ -0,0 +1,18 @@
+import { Container } from '@mui/material'
+
+/**
+ * a "ping" like route to make sure the server is up and using the correct base path
+ */
+const PathPage = ({ path }: { path: string }) => {
+  return <Container>Url: {path}</Container>
+}
+
+export default PathPage
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      path: process.env.NEXT_BASE_PATH,
+    },
+  }
+}
