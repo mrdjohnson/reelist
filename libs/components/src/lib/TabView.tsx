0c50853a68fbd090fde3247adacf28187fc0ffdf
djohnson <mr.demarcus.johnson@gmail.com>
2023-04-28 12:37:32 -0700

    ui init TabView: component that allows basic tabs and switching between them

---
+++ libs/components/src/lib/TabView.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement, useState } from 'react'
+import { Pressable, Row, Text, View } from 'native-base'
+
+type TabViewProps = {
+  routes: Array<{ name: string; render: () => ReactElement }>
+}
+
+const TabView = ({ routes }: TabViewProps) => {
+  const [tabIndex, setTabIndex] = useState(0)
+
+  return (
+    <View>
+      <Row>
+        {routes.map((route, i) => {
+          const color = tabIndex === i ? 'transparent' : 'blue.300:alpha.30'
+
+          return (
+            <Pressable
+              key={route.name}
+              padding="2"
+              alignItems="center"
+              flex={1}
+              onPress={() => setTabIndex(i)}
+              borderBottomWidth="3"
+              borderColor={color}
+              backgroundColor={color}
+            >
+              <Text>{route.name}</Text>
+            </Pressable>
+          )
+        })}
+      </Row>
+
+      {routes[tabIndex].render()}
+    </View>
+  )
+}
+
+export default TabView
