1fcdeb7dfdd64ca7aea97020cfbe22873bb7adfe
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-04 21:31:58 +0900

    VideoModal: include cast in the response

--- apps/server/components/video/VideoModal.tsx
+++ apps/server/components/video/VideoModal.tsx
@@ -56,29 +56,57 @@ const VideoModal = observer(
 
           <div className="whitespace-normal break-words">{video.overview}</div>
 
-          <div className="flex flex-1 items-end pt-4 w-full">
-            <div className="w-full">
-              <div className="text-2xl pb-3">
-                {providers.length === 0 ? 'Not available in provided regions' : 'Available on'}
+          <div className="flex flex-1 items-end pt-4 w-full ">
+            <div className="flex-col w-full">
+              <div className="w-full relative overscroll-x-none no-scrollbar overflow-x-auto">
+                <div className="text-2xl pb-3 sticky w-full left-0 z-20">Cast</div>
+
+                <div className="flex gap-x-5  pb-4 relative">
+                  {video.cast.map(
+                    castMember =>
+                      castMember.profilePath && (
+                        <div className="flex flex-col justify-center text-center">
+                          <img
+                            src={IMAGE_PATH + castMember.profilePath}
+                            className="rounded-md object-cover mb-3"
+                            alt={castMember.name}
+                            width="100px"
+                            height="100px"
+                          />
+
+                          <span className="text-base line-clamp-1">{castMember.character}</span>
+                          <span className="text-sm opacity-75 line-clamp-2  h-[2.50rem]">
+                            {castMember.name}
+                          </span>
+                        </div>
+                      ),
+                  )}
+                </div>
               </div>
 
-              <div
-                className="flex gap-x-5 xl:gap-x-8 overscroll-none no-scrollbar overflow-x-auto pb-2"
-                style={{ scrollbarWidth: 'none' }}
-              >
-                {providers.map(provider => (
-                  <div className="flex flex-col justify-center text-center">
-                    <img
-                      src={IMAGE_PATH + provider.logoPath}
-                      className="rounded-md object-contain mb-3"
-                      alt={provider.providerName}
-                      width="50px"
-                      height="50px"
-                    />
+              <div className="w-full">
+                <div className="text-2xl pb-3">
+                  {providers.length === 0 ? 'Not available in provided regions' : 'Available on'}
+                </div>
 
-                    <span>{provider.type}</span>
-                  </div>
-                ))}
+                <div
+                  className="flex gap-x-5 xl:gap-x-8 overscroll-x-none no-scrollbar overflow-x-auto pb-2"
+                  style={{ scrollbarWidth: 'none' }}
+                >
+                  {providers.map(provider => (
+                    <div className="flex flex-col justify-center text-center">
+                      <img
+                        src={IMAGE_PATH + provider.logoPath}
+                        className="rounded-md object-contain mb-3"
+                        alt={provider.providerName}
+                        width="50px"
+                        height="50px"
+                      />
+
+                      <span>{provider.type}</span>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
