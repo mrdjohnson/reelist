445f1c3b307d194be627b9cd432a5b8a08352c00
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-07 09:56:10 +0900

    wip remove native base from server

--- apps/server/next.config.js
+++ apps/server/next.config.js
@@ -2,7 +2,7 @@ const env = process.env.NODE_ENV || 'development'
 const basePath = process.env.BASE_PATH || undefined
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNativebase } = require('@native-base/next-adapter');
+const { withNx } = require('@nrwl/next/plugins/with-nx')
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -17,4 +17,4 @@ const nextConfig = {
   basePath,
 }
 
-module.exports = withNativebase(nextConfig)
+module.exports = withNx(nextConfig)
--- apps/server/pages/_document.tsx
+++ apps/server/pages/_document.tsx
@@ -1,1 +1,17 @@
-export { default } from '@native-base/next-adapter/document'
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Reelist</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
