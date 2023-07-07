d093c923cb7998008d8f06fdab0b7cd86625943a
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-07 10:18:34 +0900

    wip

--- apps/server/pages/logout.tsx
+++ apps/server/pages/logout.tsx
@@ -1,28 +1,28 @@
-import { supabaseClient } from '../utils/supabaseClient'
+// import { supabaseClient } from '../utils/supabaseClient'
 import { useEffect, useState } from 'react'
 import Router from 'next/router'
 import { useUser } from '@supabase/auth-helpers-react'
 import { Box, Button } from '@mui/material'
 
 const Logout = () => {
-  const { user, error } = useUser()
-  const [message, setMessage] = useState<string>('')
+  // const { user, error } = useUser()
+  // const [message, setMessage] = useState<string>('')
 
-  useEffect(() => {
-    if (user) {
-      supabaseClient.auth.signOut()
-      setMessage('logged out ' + user.email)
-      Router.push('/')
-    } else if (error) {
-      setMessage('custom error: ' + error.message)
-    } else {
-      setMessage('no one logged in')
-    }
-  }, [user, error])
+  // useEffect(() => {
+  //   if (user) {
+  //     supabaseClient.auth.signOut()
+  //     setMessage('logged out ' + user.email)
+  //     Router.push('/')
+  //   } else if (error) {
+  //     setMessage('custom error: ' + error.message)
+  //   } else {
+  //     setMessage('no one logged in')
+  //   }
+  // }, [user, error])
 
   return (
     <Box>
-      {message}
+      {/* {message} */}
 
       <Button href="/">Home</Button>
     </Box>
