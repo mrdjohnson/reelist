c899dd74b3631d44cd2d094ae2651282d4147060
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-09 09:49:12 +0900

    create install script to be added to the server

---
+++ apps/server/scripts/docker/on_server/install.mjs
@@ -0,0 +1,39 @@
+import { spawn } from 'node:child_process'
+
+const printSpawnOutput = commandString => {
+  const [command, ...options] = commandString.split(' ')
+  const commandSpawn = spawn(command, options)
+
+  console.log('------- Starting : ', commandString)
+
+  return new Promise(resolve => {
+    commandSpawn.stdout.on('data', data => {
+      console.log(data.toString())
+    })
+
+    commandSpawn.stderr.on('data', error => {
+      console.error(error.message)
+    })
+
+    commandSpawn.on('error', error => {
+      console.error(error.message)
+    })
+
+    commandSpawn.on('close', code => {
+      console.log(`------- finished with code:${code} \n\n`)
+      resolve()
+    })
+  })
+}
+
+const run = async () => {  
+  await printSpawnOutput('docker load --input reelist-server.tar')
+
+  await printSpawnOutput('docker rm -f reelist-server')
+
+  await printSpawnOutput('docker run --restart=always -d -p 3000:3000 --name reelist-server reelist-server')
+
+  await printSpawnOutput('docker ps')
+}
+
+run()
