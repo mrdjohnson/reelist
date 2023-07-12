c7a0b46af7e400828d360da104bb2fce699affdd
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-12 14:22:29 +0900

    adding tailwind customizations

--- apps/server/tailwind.config.js
+++ apps/server/tailwind.config.js
@@ -16,15 +16,18 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'reelist-gradient':
+          'radial-gradient(50% 50% at 50% 50%, rgba(21, 30, 1, 0.25) 0%, rgba(0, 0, 0, 0.45) 100%',
       },
       colors: {
         'text-red-400': 'rgb(254, 83, 101)',
         'reelist-red': '#FE5365',
+        'transparent-dark': 'rgba(0, 0, 0, 0.49)',
       },
       spacing: {
-        '500': '500px',
-        '600': '600px',
-      }
+        500: '500px',
+        600: '600px',
+      },
     },
     screens: {
       'discover-md': '674px',
