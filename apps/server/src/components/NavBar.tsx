4d32d33307969b9828a908515e6f5debf03b3498
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-12 12:22:21 +0900

    navbar should not change size based on videos

--- apps/server/src/components/NavBar.tsx
+++ apps/server/src/components/NavBar.tsx
@@ -4,23 +4,24 @@ const activeButton = 'border-red-400 border-1'
 const defaultStyle = 'text-lg text-slate-300 px-5 rounded-l-full rounded-r-full '
 
 const getButtonProps = (path: string, href: string) => {
-  const activeStyle =
-    path === href ? 'border border-solid border-red-400 ' : ''
+  const activeStyle = path === href ? 'border border-solid border-red-400 ' : ''
 
   return { className: defaultStyle + activeStyle, href }
 }
 
 const NavBar = ({ path }: { path: string }) => {
   return (
-    <div className="flex w-full mb-12 justify-between">
-      <a className="text-4xl text-slate-300 no-underline " href="/">
-        Reelist
-      </a>
+    <div className="discover-md:mb-8 mx-5 mb-4 mt-3">
+      <div className="flex w-full justify-between">
+        <a className="text-4xl text-slate-300 no-underline " href="/">
+          Reelist
+        </a>
 
-      <div>
-        <Button {...getButtonProps(path, '/')}>Home</Button>
-        <Button {...getButtonProps(path, '/about')}>About</Button>
-        <Button {...getButtonProps(path, '/discover')}>Discover</Button>
+        <div>
+          <Button {...getButtonProps(path, '/')}>Home</Button>
+          <Button {...getButtonProps(path, '/about')}>About</Button>
+          <Button {...getButtonProps(path, '/discover')}>Discover</Button>
+        </div>
       </div>
     </div>
   )
