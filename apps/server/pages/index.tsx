89196dfb16c0db233356f816926a59a30e2c814b
djohnson <mr.demarcus.johnson@gmail.com>
2023-07-26 11:38:25 +0900

    wip: cleanup discover homescreen

--- apps/server/pages/index.tsx
+++ apps/server/pages/index.tsx
@@ -13,7 +13,7 @@ const Index = () => {
         <title>Reelist</title>
       </Head>
 
-      <div className="flex w-full flex-col justify-center pb-12  text-center text-white top-[25%] fixed">
+      <div className="fixed top-[25%] flex w-full flex-col  justify-center pb-12 text-center text-white">
         <div className="pb-12">
           <div className="mb-3 text-5xl  ">Reelist</div>
 
@@ -24,7 +24,26 @@ const Index = () => {
 
           <div className="text-xl">
             Web: Find shows and movies to watch with your friends!{' '}
-            <Button href="/discover">Discover </Button>
+            <Button
+              href="/discover"
+              className="text-reelist-red border-reelist-red border border-solid hover:text-white"
+            >
+              Discover
+
+              {/* small arrow right hero-icons */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Button>
           </div>
         </div>
       </div>
