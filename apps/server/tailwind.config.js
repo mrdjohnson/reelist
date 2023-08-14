dca097d52c0e4f02f7c4550bab3ab8fd4d2ad204
djohnson <mr.demarcus.johnson@gmail.com>
2023-08-14 10:49:43 -0700

    ui tailwind config: main background and transparent darkness

--- apps/server/tailwind.config.js
+++ apps/server/tailwind.config.js
@@ -16,13 +16,13 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'reelist-gradient':
-          'radial-gradient(50% 50% at 50% 50%, rgba(21, 30, 1, 0.25) 0%, rgba(0, 0, 0, 0.45) 100%',
+        'reelist-gradient-green':
+          'radial-gradient(50% 50% at 50% 50%, #1A200F 0%, #131313 100%)',
       },
       colors: {
         'text-red-400': 'rgb(254, 83, 101)',
         'reelist-red': '#FE5365',
-        'transparent-dark': 'rgba(0, 0, 0, 0.49)',
+        'transparent-dark': 'rgba(0, 0, 0, 0.59)',
         'reelist-gray': 'rgb(19, 19, 19)',
       },
       spacing: {
