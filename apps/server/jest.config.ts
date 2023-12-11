e37507b7cd49619f64e97ae775d7276e92558309
djohnson <mr.demarcus.johnson@gmail.com>
2023-12-11 07:24:00 -0800

    chore Server: trying to get tests setup

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -2,10 +2,11 @@
 export default {
   displayName: 'server',
   preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/jestSetup.ts'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/server',
-};
+}
