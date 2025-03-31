c51b00e4f22d3b40ae6ba92eb30761be6441aa7c
djohnson <mr.demarcus.johnson@gmail.com>
2025-03-31 17:12:09 +0900

    manually setting the port for render.com

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 import tailwindcss from '@tailwindcss/vite'
 
-const PORT = Number(process.env.PORT || 5555)
+const PORT = process.env.NODE_ENV === 'production' ? 10000 : 5555
 
 export default defineConfig({
   plugins: [
