6a26f42cff3f7d1c47b9310f27830141ff9b2672
djohnson <mr.demarcus.johnson@gmail.com>
2025-07-11 11:32:40 +0200

    remove other packages

--- apps/mobile/src/components/AppButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ActionButton, { ActionButtonProps } from '~/components/ActionButton'
-
-const AppButton = ({ color, ...props }: ActionButtonProps) => {
-  return <ActionButton variant="ghost" color={color} {...props} />
-}
-
-export default AppButton
