6ec16ee0d7be8732b278613db50246f13795ff53
djohnson <mr.demarcus.johnson@gmail.com>
2025-08-01 03:04:31 +0700

    chore: update server dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -13,13 +13,6 @@ export default defineConfig({
   plugins: [
     remix({
       presets: [vercelPreset()],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
     }),
     tsconfigPaths(),
   ],
